63aec034addc8afa09fe919d2fbfb322c64025ca
Rebeca S Carmo <rebeca.scarmoo@gmail.com>
2026-03-17 12:21:33 -0300

    Update Codigo.ino
    The definition and description of the wheels

--- Codigo.ino
+++ Codigo.ino
@@ -1,75 +1,80 @@
 #include <Servo.h>
 #include <HCSR04.h>
-UltraSonicDistanceSensor distanceSensor(12, 13); 
-#define roda1H 4 //esquerdo dianteiro
-#define roda1A 5 //esquerdo traseiro
-#define roda2A 6 //direito traseiro
-#define roda2H 7 //direto dianteiro
-int pos =0;
-unsigned long tempomudanca = 0;
-Servo myservo;
+
+#define roda1FE 2
+#define roda1TE 3
+
+#define roda2FD 4
+#define roda2TD 5
+
+#define roda3FE 6
+#define roda3TE 7
+
+#define roda4FD 8
+#define roda4TD 9
+
 void setup(){
-pinMode(roda1H, OUTPUT);
-pinMode(roda1A, OUTPUT);
-pinMode(roda2H, OUTPUT);
-pinMode(roda2A, OUTPUT);
-myservo.attach(11);
-distanceSensor.measureDistanceCm(); 
+  pinMode(roda1FE,OUTPUT);
+  pinMode(roda1TE,OUTPUT);
+  pinMode(roda2FD,OUTPUT);
+  pinMode(roda2TD,OUTPUT);
+  pinMode(roda3FE,OUTPUT);
+  pinMode(roda3TE,OUTPUT);
+  pinMode(roda4FD,OUTPUT);
+  pinMode(roda4TD,OUTPUT);
+   Serial.begin(9600);
+
+}  
+
+void loop (){
+  Serial.println("rodando a roda dianteira esquerda para frente");
+  digitalWrite(roda1FE, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda1FE, LOW);
+  delay(1000);
+  Serial.println("rodando a roda dianteira esquerda para trás");
+  digitalWrite(roda1TE, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda1TE, LOW);
+  delay(1000);
+  Serial.println("rodando a roda dianteira direita para frente");
+  digitalWrite(roda2FD, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda2FD, LOW);
+  delay(1000);
+  Serial.println("rodando a roda dianteira direita para trás");
+  digitalWrite(roda2TD, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda2TD, LOW);
+  delay(1000);
+  Serial.println("rodando a roda traseira esquerda para frente");
+  digitalWrite(roda3FE, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda3FE, LOW);
+  delay(1000);
+  Serial.println("rodando a roda traseira esquerda para trás");
+  digitalWrite(roda3TE, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda3TE, LOW);
+  delay(1000);
+  Serial.println("rodando a roda traseira direita para frente");
+  digitalWrite(roda4FD, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda4FD, LOW);
+  delay(1000);
+  Serial.println("rodando a roda traseira direita para trás");
+  digitalWrite(roda4TD, HIGH);
+  delay(3000);
+  Serial.println(" ,parei");
+  digitalWrite(roda4TD, LOW);
+  delay(1000);
 }
 
-void loop(){
- while(distanceSensor.measureDistanceCm()>=10){
-   digitalWrite(roda1A, HIGH);
-   digitalWrite(roda2A, HIGH);
- }
-   digitalWrite(roda1A, LOW);
-   digitalWrite(roda2A, LOW);
-   temCoisaDosLados();
-  
-}
-void temCoisaDosLados(){
-  bool temNaDireita=false;
-  bool temNaEsquerda=false;
-  myservo.write(45);
-  if(distanceSensor.measureDistanceCm()<=10){
-    temNaDireita=true;
-  }
-  myservo.write(90);
-  myservo.write(135);
-  if(distanceSensor.measureDistanceCm()<=10){
-    temNaEsquerda=true;
-  }
-  myservo.write(90);
-  
-}
 
-void gire(bool temNaDireita, bool temNaEsquerda){
-  if(temNaDireita && temNaEsquerda){
-    digitalWrite(roda1H, HIGH);
-    digitalWrite(roda2H, HIGH);
-    if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda1H, LOW);
-      digitalWrite(roda2H, LOW);
-    }
-    temCoisaDosLados();
-  }else if(temNaDireita){
-    digitalWrite(roda1H, HIGH);
-    digitalWrite(roda2A, HIGH);
-     if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda1H, LOW);
-      digitalWrite(roda2A, LOW);
-     }
-   }else if(temNaEsquerda){
-    digitalWrite(roda2H, HIGH);
-    digitalWrite(roda1A, HIGH);
-    if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda2H, LOW);
-      digitalWrite(roda1A, LOW);
-    }
-  }
-}
-
- 
