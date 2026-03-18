2ff62a602170f00ed3a2a6c234b6b4924777e5c4
Thaylancardoso <thaylancardoso715@gmail.com>
2026-03-18 18:15:41 -0300

    Codigo.ino
    Updated code, containing small changes to the logic and adjustments to the servo motor's execution time using delay, as well as the use of the Serial.begin() function for testing the ultrasonic sensor.

--- Codigo.ino
+++ Codigo.ino
@@ -1,75 +1,137 @@
 #include <Servo.h>
 #include <HCSR04.h>
-UltraSonicDistanceSensor distanceSensor(12, 13); 
-#define roda1H 4 //esquerdo dianteiro
-#define roda1A 5 //esquerdo traseiro
-#define roda2A 6 //direito traseiro
-#define roda2H 7 //direto dianteiro
-int pos =0;
-unsigned long tempomudanca = 0;
+
+#define roda1H 4 // esquerdo dianteiro
+#define roda1A 5 // esquerdo traseiro
+#define roda2A 6 // direito traseiro
+#define roda2H 7 // direito dianteiro
+
+UltraSonicDistanceSensor distanceSensor(12, 13);
+
 Servo myservo;
-void setup(){
-pinMode(roda1H, OUTPUT);
-pinMode(roda1A, OUTPUT);
-pinMode(roda2H, OUTPUT);
-pinMode(roda2A, OUTPUT);
-myservo.attach(11);
-distanceSensor.measureDistanceCm(); 
+
+unsigned long tempomudanca = 0;
+
+int distancia;
+
+
+
+void setup() {
+
+  Serial.begin(9600); //Inicia a comunicação do arduino com o sensor
+
+  pinMode(roda1H, OUTPUT); 
+  pinMode(roda1A, OUTPUT);
+  pinMode(roda2H, OUTPUT);
+  pinMode(roda2A, OUTPUT);
+  myservo.attach(11);
+
+  myservo.write(90);
+
 }
 
-void loop(){
- while(distanceSensor.measureDistanceCm()>=10){
-   digitalWrite(roda1A, HIGH);
-   digitalWrite(roda2A, HIGH);
- }
-   digitalWrite(roda1A, LOW);
-   digitalWrite(roda2A, LOW);
-   temCoisaDosLados();
-  
+
+
+void loop() {
+
+  distancia = distanceSensor.measureDistanceCm();
+  Serial.println("Primeira medição: ";
+  Serial.println(distancia);
+
+  if (distancia >= 10) {
+
+    digitalWrite(roda1A, HIGH);
+    digitalWrite(roda2A, HIGH);
+    
+
+  } else {
+
+    digitalWrite(roda1A, LOW);
+    digitalWrite(roda2A, LOW);
+
+    temCoisaDosLados();
+
+  }
+
 }
-void temCoisaDosLados(){
-  bool temNaDireita=false;
-  bool temNaEsquerda=false;
+
+void temCoisaDosLados() {
+
+  bool temNaDireita = false;
+  bool temNaEsquerda = false;
+
   myservo.write(45);
-  if(distanceSensor.measureDistanceCm()<=10){
-    temNaDireita=true;
+  delay(300);
+
+  distancia = distanceSensor.measureDistanceCm();
+
+  Serial.print("Direita: ");
+  Serial.println(distancia);
+
+  if (distancia <= 10) {
+    temNaDireita = true;
   }
+
   myservo.write(90);
+  delay(200);
+
+  distancia = distanceSensor.measureDistanceCm();
+
+  Serial.print("Centro: ");
+  Serial.println(distancia);
+
   myservo.write(135);
-  if(distanceSensor.measureDistanceCm()<=10){
-    temNaEsquerda=true;
+  delay(300);
+
+  distancia = distanceSensor.measureDistanceCm();
+
+  Serial.print("Esquerda: ");
+  Serial.println(distancia);
+
+  if (distancia <= 10) {
+    temNaEsquerda = true;
   }
+
   myservo.write(90);
-  
+  delay(200);
+
+  gire(temNaDireita, temNaEsquerda);
 }
 
-void gire(bool temNaDireita, bool temNaEsquerda){
-  if(temNaDireita && temNaEsquerda){
+void gire(bool temNaDireita, bool temNaEsquerda) {
+
+  if (temNaDireita && temNaEsquerda) {
+
     digitalWrite(roda1H, HIGH);
     digitalWrite(roda2H, HIGH);
-    if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda1H, LOW);
-      digitalWrite(roda2H, LOW);
-    }
-    temCoisaDosLados();
-  }else if(temNaDireita){
-    digitalWrite(roda1H, HIGH);
-    digitalWrite(roda2A, HIGH);
-     if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda1H, LOW);
-      digitalWrite(roda2A, LOW);
-     }
-   }else if(temNaEsquerda){
+    delay(1000);
+
+    digitalWrite(roda1H, LOW);
+    digitalWrite(roda2H, LOW);
+
+  }
+
+  else if (temNaDireita) {
+
     digitalWrite(roda2H, HIGH);
     digitalWrite(roda1A, HIGH);
-    if(millis()-tempomudanca==1000){
-      tempomudanca=millis();
-      digitalWrite(roda2H, LOW);
-      digitalWrite(roda1A, LOW);
-    }
-  }
-}
+    delay(700);
 
- 
+    digitalWrite(roda2H, LOW);
+    digitalWrite(roda1A, LOW);
+
+  }
+
+  else if (temNaEsquerda) {
+
+    digitalWrite(roda1H, HIGH);
+    digitalWrite(roda2A, HIGH);
+    delay(700);
+
+    digitalWrite(roda1H, LOW);
+    digitalWrite(roda2A, LOW);
+
+  }
+
+  }
+
